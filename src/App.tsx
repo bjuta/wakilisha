6bd7bde8be12261213bcba6e4634f04902ec13d4
bjuta <116518818+bjuta@users.noreply.github.com>
2026-05-31 00:54:49 +0300

    Load admin design system layout CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { AppRoutes } from "./router";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 import "./design-system/wakilisha.tokens.css";
+import "./pages/admin/design-system/adminDesignSystemLayout.css";
 import { ThemeProvider } from "./components/design-system/theme/ThemeProvider";
 
 function App() {
