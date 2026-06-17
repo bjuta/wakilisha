be335e313aaba0d5f167f5b5861175e0807f40a4
bjuta <116518818+bjuta@users.noreply.github.com>
2026-06-17 18:46:49 +0300

    feat: add culture context facts facade

---
+++ src/services/cultureContext/facts.ts
@@ -0,0 +1,21 @@
+export {
+  normalizeArtistFacts,
+  normalizeChartFacts,
+  normalizeCultureFacts,
+  normalizeGenreFacts,
+  normalizeLabelFacts,
+  normalizeReleaseFacts,
+  normalizeSearchResultFacts,
+  normalizeTrackFacts,
+} from "./normalize";
+
+export type {
+  ArtistFacts,
+  ChartFacts,
+  CultureFacts,
+  GenreFacts,
+  LabelFacts,
+  ReleaseFacts,
+  SearchResultFacts,
+  TrackFacts,
+} from "./types";
