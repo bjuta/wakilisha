872e34d30fb637fa3e6120858d37d6c87022cf51
bjuta <116518818+bjuta@users.noreply.github.com>
2026-06-17 20:52:30 +0300

    Point issue one editorial paths to MDX files

--- src/services/magazineIssueEngine/editorialPaths.ts
+++ src/services/magazineIssueEngine/editorialPaths.ts
@@ -1,5 +1,7 @@
+const ISSUE_ONE_DIR = ['src', 'content', 'magazine', 'issues', 'issue-001'].join('/');
+
 export const MAGAZINE_EDITORIAL_PATHS = {
-  editorNote: 'editor-note',
-  coverStatement: 'cover-statement',
-  backMatter: 'back-matter',
+  editorNote: `${ISSUE_ONE_DIR}/editor-note.mdx`,
+  coverStatement: `${ISSUE_ONE_DIR}/cover-statement.mdx`,
+  backMatter: `${ISSUE_ONE_DIR}/back-matter.mdx`,
 };
